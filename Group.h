154474b53a039bd2d7d4e1a98159c74dee3ce997
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-06 18:50:48 -0600

    fill out group

--- Group.h
+++ Group.h
@@ -21,27 +21,43 @@ public:
   }
 	
   Group( int num_objects ){
+    objects = new Object3D*[num_objects];
+    num = num_objects;
 
+    for (int i = 0; i < num; i++)
+    {
+      objects[i] = nullptr;
+    }
   }
 
   ~Group(){
-   
+    for (int i = 0; i < num; i++)
+    {
+      delete objects[i];
+    }
+
+    delete[] objects;
   }
 
   virtual bool intersect( const Ray& r , Hit& h , float tmin ) {
-		
-   }
+    for (int i = 0; i < num; i++)
+    {
+      objects[i]->intersect(r, h, tmin);
+    }
+  }
 	
   void addObject( int index , Object3D* obj ){
-
+    objects[index] = obj;
   }
 
   int getGroupSize(){ 
-  
+    return num;
   }
 
  private:
-
+    Object3D** objects = nullptr;
+    int num = 0;
+    
 };
 
 #endif
