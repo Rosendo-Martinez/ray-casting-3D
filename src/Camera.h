8aa524288b9748f4ded303193c17f34b1e759bb2
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-12 16:46:48 -0600

    clean up Cam

--- src/Camera.h
+++ src/Camera.h
@@ -11,70 +11,52 @@ class Camera
 {
 public:
 	//generate rays for each screen-space coordinate
-	virtual Ray generateRay( const Vector2f& point ) = 0 ; 
+	virtual Ray generateRay(const Vector2f& point) = 0; 
 	
-	virtual float getTMin() const = 0 ; 
-	virtual ~Camera(){}
+	virtual float getTMin() const = 0; 
+	virtual ~Camera() {}
+
 protected:
 	Vector3f center; 
 	Vector3f direction;
 	Vector3f up;
 	Vector3f horizontal;
-
 };
 
-///TODO: Implement Perspective camera
-///Fill in functions and add more fields if necessary
 class PerspectiveCamera: public Camera
 {
 public:
-	PerspectiveCamera(const Vector3f& center, const Vector3f& direction,const Vector3f& up , float angle){
-		this->center = center;
-		this->fov = angle;
+	PerspectiveCamera(const Vector3f& center, const Vector3f& direction, const Vector3f& up, float angle)
+	{
+		this->center = center; // camera position
+		this->fov = angle; // radians, horizontal
+		this->direction = direction; // looking at
 
-		// Camera basis vectors
-		this->direction = direction;
+		// Calculate camera basis vectors
 		this->horizontal = Vector3f::cross(this->direction, up).normalized();
 		this->up = Vector3f::cross(this->horizontal, this->direction).normalized();
 
-		// Virtual screen
-		this->screenOffset = direction * (1.0f / tan(this->fov/2.0f));
-
-		std::cout << "Angle: " << angle << '\n';
-		std::cout << "Camera Center: ";
-		this->center.print();
-		std::cout << "Camera Direction: ";
-		this->direction.print();
-		std::cout << "Camera Horizontal: ";
-		this->horizontal.print();
-		std::cout << "Camera Up: ";
-		this->up.print();
-		std::cout << "Screen Offset: ";
-		this->screenOffset.print();
+		// Calculate virtual screen offset
+		this->virtualScreenOffset = direction * (1.0f / tan(this->fov/2.0f));
 	}
 
-	virtual Ray generateRay( const Vector2f& point){
-		// ASSUMPTION: No aspect ratio? I assume image will be square, for now.
-		Vector3f pointOnScreen = (point.x() * this->horizontal) + (point.y() * this->up) + screenOffset + this->center;
+	// ASSUMPTION: Only handles square images, for now.
+	virtual Ray generateRay(const Vector2f& point)
+	{
+		Vector3f pointOnScreen = (point.x() * this->horizontal) + (point.y() * this->up) + virtualScreenOffset + this->center;
 		Vector3f rayDir = (pointOnScreen - this->center).normalized();
-		Ray ray = Ray(this->center, rayDir);
 
-		// std::cout << "Point: "; point.print();
-		// std::cout << "Point On Screen: "; pointOnScreen.print();
-		// std::cout << "Ray Dir: "; ray.getDirection().print();
-		// std::cout << "Ray Org: "; ray.getOrigin().print();
-
-		return ray;
+		return Ray(this->center, rayDir);
 	}
 
-	virtual float getTMin() const { 
+	virtual float getTMin() const
+	{ 
 		return 0.0f;
 	}
 
 private:
-	float fov; // horizontal
-	Vector3f screenOffset;
-
+	float fov; // radians, horizontal
+	Vector3f virtualScreenOffset;
 };
 
 #endif //CAMERA_H
