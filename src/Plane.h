f3312246d7b0662463abf03191f845b22f4b603e
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-14 14:10:42 -0600

    Plane, but has bugs

--- src/Plane.h
+++ src/Plane.h
@@ -5,22 +5,46 @@
 #include <vecmath.h>
 #include <cmath>
 using namespace std;
-///TODO: Implement Plane representing an infinite plane
-///choose your representation , add more fields and fill in the functions
-class Plane: public Object3D
+
+
+class Plane : public Object3D
 {
 public:
-	Plane(){}
-	Plane( const Vector3f& normal , float d , Material* m):Object3D(m){
-	}
-	~Plane(){}
-	virtual bool intersect( const Ray& r , Hit& h , float tmin){
-		// TODO
+	Plane() {}
+
+	Plane(const Vector3f& normal, float d, Material* m) 
+		: Object3D(m), normal(normal), d(d)
+	{}
+
+	~Plane() {}
+
+	virtual bool intersect(const Ray& r, Hit& h, float tmin)
+	{
+		float n_dot_rd = Vector3f::dot(this->normal, r.getDirection());
+
+		bool isRayDirectionParallelToPlan = (n_dot_rd == 0);
+		if (isRayDirectionParallelToPlan) // many or zero intersections
+		{
+			return false;
+		}
+
+		float t = -(this->d + Vector3f::dot(this->normal, r.getOrigin())) / n_dot_rd;
+		
+		bool isLegalIntersection = (t >= 0.0f && t >= tmin);
+		bool isCloserIntersection = (t < h.getT());
+		if (isLegalIntersection && isCloserIntersection)
+		{
+			// In some cases normal may need to be negative! 
+			h.set(t, this->material, this->normal);
+			return true;
+		}
+
 		return false;
 	}
 
 protected:
+	const Vector3f normal = Vector3f(0.0f);
+	const float d = 0.0f;
 };
-#endif //PLANE_H
-		
 
+#endif //PLANE_H
